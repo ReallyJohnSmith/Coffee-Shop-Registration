bfc03fc594fcfff1cf51e466bca67a3b1c68ded3
ReallyJohnSmith <reallyjohnsmith21@gmail.com>
2022-07-13 18:10:57 -0400

    Add project files.

---
+++ Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Coffee_Shop_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Results()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult Registration(string FirstName, string LastName, string Email, string Password)
+        {
+            ViewData["UserFirstName"] = FirstName;
+            ViewData["UserLastName"] = LastName;
+            ViewData["UserEmail"] = Email;
+            ViewData["Password"] = Password;
+
+
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public class Results
+    {
+
+    }
+
+    public class Registration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Registration()
+        {
+
+        }
+    }
+}
